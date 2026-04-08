3260bcceccded58f914613b45e470a66f1ca96de
Theodore <theodore@email.com>
2026-04-07 22:10:15 -0400

    adding the functionality to delete an anime

--- src/Components/animeList.jsx
+++ src/Components/animeList.jsx
@@ -1,10 +1,22 @@
 import "./animeList.css";
+import DeleteAnime from "./DeleteAnime";
+
+// import { Edit } from "lucide-react";
 import defaultCover from "../assets/allanime.jpeg";
 
-export default function AnimeList({ titre, statut, genre, episodes, cover }) {
+export default function AnimeList({
+  id,
+  titre,
+  statut,
+  genre,
+  episodes,
+  cover,
+  onDelete,
+}) {
   return (
     <section className="card">
       <h3>{titre}</h3>
+
       <div>
         <img src={cover || defaultCover}></img>
         <p>
@@ -16,6 +28,9 @@ export default function AnimeList({ titre, statut, genre, episodes, cover }) {
         <p>
           <span className="before">Statut</span> : {statut}
         </p>
+        <div className="card-header">
+          <DeleteAnime id={id} onDelete={onDelete} />
+        </div>
       </div>
     </section>
   );
